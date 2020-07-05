ca19647862dd89659ffe26972f9f73280d1e138d
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-07-05 18:48:33 -0300

    Model Appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,11 @@
 import { Router, response } from 'express';
-import { uuid } from 'uuidv4';
 // startOfHour zera a hora em que se está sendo feito o agendamento
 // parseISO converte a data do insomnia para uma Date do javascript
 //isEqual é para procurar no banco uma data igual a inserida
-import { startOfHour, parseISO, isEqual } from 'date-fns'
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-    id: string;
-    provider: string;
-    date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -29,11 +23,7 @@ appointmentsRouter.post('/', (request, response) => {
         return response.status(400).json({message: 'This appointment is already booked'})
     }
 
-    const appointment = {
-        id: uuid(),
-        provider,
-        date: parsedDate,
-    };
+    const appointment = new Appointment(provider, parsedDate);
 
     appointments.push(appointment);
     return response.json(appointment);
