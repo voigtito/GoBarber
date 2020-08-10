ce664ef0317cccc422c608ecfe8d80c0d7c22f64
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-08-10 18:18:58 -0300

    Adding client folder

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index'
+import SignUp from './pages/SignUp/index'
+
+const App: React.FC = () => (
+  <>
+    <SignUp/>
+    <GlobalStyle />
+  </>
+)
+
+export default App;
---
+++ client/src/components/input/index.tsx
@@ -0,0 +1,30 @@
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import { Container } from './style';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    name: string;
+    icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+    const inputRef = useRef(null);
+    const { fieldName, defaultValue, error, registerField } = useField(name);
+    
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value',
+        });
+    }, [fieldName, registerField]);
+    return (
+        <Container>
+            {Icon && <Icon size={20} />}
+            <input defaultValue={defaultValue} ref={inputRef} {...rest}></input>
+        </Container>
+    )
+};
+
+export default Input;
