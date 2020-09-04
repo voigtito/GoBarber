607913a3018e9ff9ae2f31348fe0916f3ca43c32
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-09-03 21:29:14 -0300

    Fetching data from form with unform

--- mobile/appgobarber/src/components/Input/index.tsx
+++ mobile/appgobarber/src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core'
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,17 +9,48 @@ interface InputProps extends TextInputProps {
     icon: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
-    <>
+interface InputValueReference {
+    value: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+    const inputElementRef = useRef<any>(null);
+
+    const { registerField, defaultValue = '', fieldName, error} = useField(name);
+    const inputValueRef = useRef<InputValueReference>({value: defaultValue});
+
+    // Usa-se o useEffect para que assim que o elemento seja exibido em tela, registre ele dentro do unform
+    useEffect(() => {
+        registerField<string>({
+            name: fieldName,
+            ref: inputValueRef.current,
+            path: 'value',
+            setValue(ref: any, value) {
+                inputValueRef.current.value = value;
+                inputElementRef.current.setNativeProps({ text: value});
+            },
+            clearValue() {
+                inputValueRef.current.value = '';
+                inputElementRef.current.clear();
+            }
+        });
+    }, [fieldName, registerField])
+
+    return (<>
         <Container>
-            <Icon name={icon} size={20} color="#666360"/>
+            <Icon name={icon} size={20} color="#666360" />
             <TextInput
+                ref={inputElementRef}
                 keyboardAppearance="dark"
                 placeholderTextColor="#666360"
-                {... rest}
+                defaultValue = {defaultValue}
+                onChangeText={(value) => {
+                    inputValueRef.current.value = value;
+                }}
+                {...rest}
             />
         </Container>
-    </>
-);
+    </>)
+};
 
 export default Input;
--- mobile/appgobarber/src/pages/SignIn/index.tsx
+++ mobile/appgobarber/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -11,43 +13,61 @@ import logoImg from '../../assets/Logo.png';
 import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 const SignIn: React.FC = () => {
-
+    const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
-    
+
+    const handleSignIn = useCallback((data: object) => {
+        console.log(data)
+    }, []);
+
     return (
-    <>
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-            enabled
-        >
-            <ScrollView
-                contentContainerStyle={{flex:1}}
-                keyboardShouldPersistTaps="handled"
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
             >
-                <Container>
-                    <Image source={logoImg} />
+                <ScrollView
+                    contentContainerStyle={{ flex: 1 }}
+                    keyboardShouldPersistTaps="handled"
+                >
+                    <Container>
+                        <Image source={logoImg} />
 
-                    <View>
-                        <Title>Faça seu Logon</Title>
-                    </View>
+                        <View>
+                            <Title>Faça seu Logon</Title>
+                        </View>
+                        <Form
+                            onSubmit={handleSignIn}
+                            ref={formRef}
+                        >
+                            <Input
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                            />
+                            <Input name="password" icon="lock" placeholder="Senha" />
 
-                    <Input name="email" icon="mail" placeholder="E-mail" />
-                    <Input name="password" icon="lock" placeholder="Senha" />
+                            <Button
+                                onPress={() => {
+                                    formRef.current?.submitForm();
+                                }}
+                            >
+                                Entrar
+                        </Button>
+                        </Form>
+                        <ForgotPassword onPress={() => { }}>
+                            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
 
-                    <Button onPress={() => { console.log('pressionado') }}>Entrar</Button>
-                    <ForgotPassword onPress={() => { }}>
-                        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-                    </ForgotPassword>
-                </Container>
-            </ScrollView>
-        </KeyboardAvoidingView>
-
-        <CreateAccountButton onPress={() => { navigation.navigate('SignUp')}}>
-            <Icon name="log-in" size={20} color="#ff9000" />
-            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
-        </CreateAccountButton>
-    </>
+            <CreateAccountButton onPress={() => { navigation.navigate('SignUp') }}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
     )
 };
 
