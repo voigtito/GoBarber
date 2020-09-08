8b2c4ecd8992a25747aa951674be56e29747c0e2
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-09-08 16:49:01 -0300

    Isolating Repository

--- server/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
-import Appointment from '../infra/typeorm/entities/Appointments'
+import Appointment from '../infra/typeorm/entities/Appointments';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+    create({}: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date):Promise<Appointment | undefined>
 }
