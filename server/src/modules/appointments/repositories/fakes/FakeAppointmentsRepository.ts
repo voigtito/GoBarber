7175804016427384343c9708e50a68fb2f6ca311
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-09-09 09:16:24 -0300

    First Service Test

---
+++ server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,38 @@
+import Appointment from '../../infra/typeorm/entities/Appointments';
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+
+class AppointmentsRepository implements IAppointmentsRepository {
+    // use just javascript, without database
+    private appointments: Appointment[] = [];
+
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+        const findAppointment = this.appointments.find(appointment =>
+            appointment.date === date,
+        );
+
+        return findAppointment;
+
+    }
+
+    public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
+
+        const appointment = new Appointment();
+
+        Object.assign(appointment, { id: uuid(), date, provider_id });
+
+        // Object assign é a mesma coisa que:
+        // appointment.id = uuid();
+        // appointment.date = date;
+        // appointment.provider_id = provider_id;
+
+        this.appointments.push(appointment);
+
+        return appointment;
+
+    }
+}
+
+export default AppointmentsRepository;
