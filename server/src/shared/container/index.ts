3365f947c0a992217eaa4c9e9550eb68245e7e73
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-09-14 08:31:38 -0300

    Creating Table from ResetPassword and Routes

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -8,10 +8,12 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 
-// import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
-// import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
 
 
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository);
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+
+container.registerSingleton<IUserTokenRepository>('UserTokenRepository', UserTokenRepository);
